2bb8432c51c30ac5b1e6e35026f4dddbb83bfc6a
EnriquezWandererCoder <147671506+NeonWandererCoder@users.noreply.github.com>
2026-03-14 18:14:35 +0800

    07 Task Performance 1 - ARG
    07 Task Performance 1 - ARG

---
+++ WordGuessApplication/WordGuessApplication/frmGuessWord.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace WordGuessApplication
+{
+    partial class frmGuessWord
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblWord = new System.Windows.Forms.Label();
+            this.txtGuess = new System.Windows.Forms.TextBox();
+            this.btnGuess = new System.Windows.Forms.Button();
+            this.lblWrongGuess = new System.Windows.Forms.Label();
+            this.lstWrongGuess = new System.Windows.Forms.ListBox();
+            this.lblScore = new System.Windows.Forms.Label();
+            this.lblAttempts = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblWord
+            // 
+            this.lblWord.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblWord.Location = new System.Drawing.Point(109, 40);
+            this.lblWord.Name = "lblWord";
+            this.lblWord.Size = new System.Drawing.Size(250, 37);
+            this.lblWord.TabIndex = 0;
+            this.lblWord.Text = "GUESS WORD";
+            this.lblWord.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // txtGuess
+            // 
+            this.txtGuess.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtGuess.Location = new System.Drawing.Point(107, 101);
+            this.txtGuess.Name = "txtGuess";
+            this.txtGuess.Size = new System.Drawing.Size(262, 29);
+            this.txtGuess.TabIndex = 1;
+            // 
+            // btnGuess
+            // 
+            this.btnGuess.Location = new System.Drawing.Point(152, 153);
+            this.btnGuess.Name = "btnGuess";
+            this.btnGuess.Size = new System.Drawing.Size(140, 35);
+            this.btnGuess.TabIndex = 2;
+            this.btnGuess.Text = "Guess";
+            this.btnGuess.UseVisualStyleBackColor = true;
+            this.btnGuess.Click += new System.EventHandler(this.btnGuess_Click);
+            // 
+            // lblWrongGuess
+            // 
+            this.lblWrongGuess.AutoSize = true;
+            this.lblWrongGuess.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblWrongGuess.ForeColor = System.Drawing.Color.Red;
+            this.lblWrongGuess.Location = new System.Drawing.Point(474, 9);
+            this.lblWrongGuess.Name = "lblWrongGuess";
+            this.lblWrongGuess.Size = new System.Drawing.Size(106, 17);
+            this.lblWrongGuess.TabIndex = 3;
+            this.lblWrongGuess.Text = "Wrong Guess";
+            // 
+            // lstWrongGuess
+            // 
+            this.lstWrongGuess.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lstWrongGuess.FormattingEnabled = true;
+            this.lstWrongGuess.ItemHeight = 16;
+            this.lstWrongGuess.Location = new System.Drawing.Point(460, 40);
+            this.lstWrongGuess.Name = "lstWrongGuess";
+            this.lstWrongGuess.Size = new System.Drawing.Size(142, 148);
+            this.lstWrongGuess.TabIndex = 4;
+            // 
+            // lblScore
+            // 
+            this.lblScore.AutoSize = true;
+            this.lblScore.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblScore.Location = new System.Drawing.Point(12, 9);
+            this.lblScore.Name = "lblScore";
+            this.lblScore.Size = new System.Drawing.Size(50, 17);
+            this.lblScore.TabIndex = 5;
+            this.lblScore.Text = "Score";
+            // 
+            // lblAttempts
+            // 
+            this.lblAttempts.AutoSize = true;
+            this.lblAttempts.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAttempts.Location = new System.Drawing.Point(457, 191);
+            this.lblAttempts.Name = "lblAttempts";
+            this.lblAttempts.Size = new System.Drawing.Size(100, 17);
+            this.lblAttempts.TabIndex = 6;
+            this.lblAttempts.Text = "Total Attemp";
+            // 
+            // frmGuessWord
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(604, 211);
+            this.Controls.Add(this.lblAttempts);
+            this.Controls.Add(this.lblScore);
+            this.Controls.Add(this.lstWrongGuess);
+            this.Controls.Add(this.lblWrongGuess);
+            this.Controls.Add(this.btnGuess);
+            this.Controls.Add(this.txtGuess);
+            this.Controls.Add(this.lblWord);
+            this.Name = "frmGuessWord";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Guess the Word";
+            this.Load += new System.EventHandler(this.frmGuessWord_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblWord;
+        private System.Windows.Forms.TextBox txtGuess;
+        private System.Windows.Forms.Button btnGuess;
+        private System.Windows.Forms.Label lblWrongGuess;
+        private System.Windows.Forms.ListBox lstWrongGuess;
+        private System.Windows.Forms.Label lblScore;
+        private System.Windows.Forms.Label lblAttempts;
+    }
+}
+
---
+++ WordGuessApplication/WordGuessApplication/frmGuessWord.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WordGuessApplication
+{
+    public partial class frmGuessWord : Form
+    {
+        // Array of words to guess (multiple words)
+        string[] words = { "computer", "keyboard", "monitor", "programming", "software" };
+
+        // Track the current word index
+        int currentWordIndex = 0;
+
+        // Score tracking
+        int correctCount = 0;
+        int wrongCount = 0;
+
+        // Attempts limit per word
+        int maxAttempts = 5;
+        int currentAttempts = 0;
+
+        public frmGuessWord()
+        {
+            InitializeComponent();
+        }
+
+        // Form Load event - set up the first word
+        private void frmGuessWord_Load(object sender, EventArgs e)
+        {
+            // Display the hint for the first word
+            DisplayHint();
+            UpdateScoreLabel();
+            UpdateAttemptsLabel();
+        }
+
+        // Method to display the hint using StringBuilder
+        private void DisplayHint()
+        {
+            string currentWord = words[currentWordIndex];
+
+            // Use StringBuilder to build the hint
+            StringBuilder hint = new StringBuilder();
+
+            for (int i = 0; i < currentWord.Length; i++)
+            {
+                // Show first and last letter, hide the rest with '?'
+                if (i == 0 || i == currentWord.Length - 1)
+                {
+                    hint.Append(currentWord[i]);
+                }
+                else
+                {
+                    hint.Append("?");
+                }
+            }
+
+            // Display the hint in the label
+            lblWord.Text = hint.ToString();
+        }
+
+        // Method to update the score label
+        private void UpdateScoreLabel()
+        {
+            lblScore.Text = "Correct: " + correctCount + "  |  Wrong: " + wrongCount;
+        }
+
+        // Method to update the attempts label
+        private void UpdateAttemptsLabel()
+        {
+            int remaining = maxAttempts - currentAttempts;
+            lblAttempts.Text = "Attempts left: " + remaining + " / " + maxAttempts;
+        }
+
+        // Method to move to the next word
+        private void NextWord()
+        {
+            currentWordIndex++;
+            currentAttempts = 0;
+            lstWrongGuess.Items.Clear();
+
+            // Check if there are more words
+            if (currentWordIndex < words.Length)
+            {
+                DisplayHint();
+                UpdateAttemptsLabel();
+            }
+            else
+            {
+                
+                btnGuess.Enabled = false;
+                txtGuess.Enabled = false;
+
+                if(correctCount == words.Length)
+                {
+                    // All words are done - show final score
+                    lblWord.Text = "You WON!";
+                    lblAttempts.Text = "Perfect score!";
+
+                    MessageBox.Show("Congratulations, You WON!\n\n"
+                    + "Correct guesses: " + correctCount + "\n"
+                    + "Wrong guesses: " + wrongCount + "\n"
+                    + "Total words: " + words.Length,
+                    "Final Score");
+                }
+                else
+                {
+                    // All words are done - show final score
+                    lblWord.Text = "GAME OVER";
+                    lblAttempts.Text = "No more words!";
+
+                    MessageBox.Show("Game Over!\n\n"
+                    + "Correct guesses: " + correctCount + "\n"
+                    + "Wrong guesses: " + wrongCount + "\n"
+                    + "Total words: " + words.Length,
+                    "Final Score");
+                }
+                
+            }
+        }
+
+        // Button click event - check if the guess is correct
+        private void btnGuess_Click(object sender, EventArgs e)
+        {
+            // Get the user's guess and convert to lowercase using String methods
+            string userGuess = txtGuess.Text.Trim().ToLower();
+            string currentWord = words[currentWordIndex];
+
+            // Validate that the textbox is not empty
+            if (string.IsNullOrWhiteSpace(userGuess))
+            {
+                MessageBox.Show("Please enter a word.");
+                txtGuess.Focus();
+                return;
+            }
+
+            // Compare the guess with the current word using String.Equals method
+            if (userGuess.Equals(currentWord))
+            {
+                // Correct guess - reveal the full word
+                lblWord.Text = currentWord;
+                correctCount++;
+                UpdateScoreLabel();
+
+                MessageBox.Show("Correct guess!\nMoving to the next word...");
+
+                // Move to the next word
+                NextWord();
+            }
+            else
+            {
+                // Wrong guess - add to the listbox
+                currentAttempts++;
+                wrongCount++;
+                lstWrongGuess.Items.Add(userGuess);
+                UpdateScoreLabel();
+                UpdateAttemptsLabel();
+
+                // Check if attempts are used up
+                if (currentAttempts >= maxAttempts)
+                {
+                    MessageBox.Show("Out of attempts!\nThe word was: " + currentWord
+                        + "\n\nMoving to the next word...");
+
+                    // Move to the next word
+                    NextWord();
+                }
+                else
+                {
+                    int remaining = maxAttempts - currentAttempts;
+                    MessageBox.Show("Wrong guess!\nTry again. " + remaining + " attempts left.");
+                }
+            }
+
+            // Clear the textbox for the next guess
+            txtGuess.Clear();
+            txtGuess.Focus();
+        }
+    }
+}
